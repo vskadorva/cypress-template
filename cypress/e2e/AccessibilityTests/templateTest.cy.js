874c49eba78c1c0dbd2aaa501787623086d2e565
Vitaly Skadorva <vitaly@Vitalys-MacBook-Pro.local>
2023-02-28 15:54:47 -0500

    cypress templates with accessibility using cypress-axe

---
+++ cypress/e2e/AccessibilityTests/templateTest.cy.js
@@ -0,0 +1,23 @@
+describe('template Test suite', () => {
+    before("Run before all tests", ()=> {
+
+    })
+    after("Run after all tests", ()=> {
+
+    })
+    beforeEach("Run before Each test case", ()=> {
+
+    })
+    afterEach("Run after each test case", ()=> {
+
+    })
+    it('runs test case with filter <smoke>',  ()=> {
+
+    })
+    it('runs test case without any filter', ()=> {
+        cy.visit('https://example.cypress.io')
+        cy.injectAxe();
+        cy.checkA11y();
+    })
+
+})
---
+++ cypress/e2e/UI tests/templateTest.cy.js
@@ -0,0 +1,20 @@
+describe('template Test suite', () => {
+    before("Run before all tests", ()=> {
+
+    })
+    after("Run after all tests", ()=> {
+
+    })
+    beforeEach("Run before Each test case", ()=> {
+
+    })
+    afterEach("Run after each test case", ()=> {
+
+    })
+    it('runs test case with filter <smoke>',  ()=> {
+
+    })
+    it('runs test case without any filter', ()=> {
+
+    })
+})
